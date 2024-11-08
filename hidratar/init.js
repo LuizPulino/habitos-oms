d7981d9b089902e1b0a12050b4b6ffd6d98ccb25
Luiz Pulino <luizpulino@gmail.com>
2024-11-08 11:06:46 -0300

    main.js

--- hidratar/init.js
+++
@@ -1,5 +0,0 @@
-let app = {
-    name: "Hidratar"
-};
-// set page title
-$("head>title, header>h1").text(app.name);
--- planejar/init.js
+++
@@ -1,5 +0,0 @@
-let app = {
-    name: "Planejar"
-};
-// set page title
-$("head>title, header>h1").text(app.name);
